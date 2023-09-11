edd1745ee383e8cca4de304478867c3f03e1e389
tshimsthedoor <tshims79@gmail.com>
2023-09-11 23:01:50 +0200

    eish

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 
 
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 
--- src/test/java/test/FacebookDemo.java
+++ src/test/java/test/FacebookDemo.java
@@ -2,30 +2,51 @@ package test;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeSuite;
+
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterSuite;
 
 public class FacebookDemo {
+	
+	WebDriver driver;
 
 	@BeforeSuite
 	public void beforeSuite() {
-		WebDriver driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("http://www.facebook.com");
 		System.out.println(driver.getTitle());
-		driver.findElement(By.id("email")).sendKeys("tshimsthedor@yahoo.fr");
-		driver.findElement(By.id("pass")).sendKeys("thedwer");
-		driver.findElement(By.xpath("//button[@name='login']")).click();
+		
+	}
+	
+	@Test
+	public void pageTitleValidation() {
+		String expectedResult = "Facebook – log in or sign up";
+		String actualResult = driver.getTitle();
+		
+		Reporter.log("Expected Result = " + expectedResult);
+		Reporter.log("Actual Result = " + actualResult);
+		
+		assertTrue(actualResult.equals(expectedResult), "Mistmatch in the page title.");
 	}
 
 	@Test
 	public void loginTest() {
+		driver.findElement(By.id("email")).sendKeys("tshimsthedor@yahoo.fr");
+		driver.findElement(By.id("pass")).sendKeys("thedwer");
+		driver.findElement(By.xpath("//button[@name='login']")).click();
 	}
+	
+
 
 	@AfterSuite
 	public void afterSuite() {
+		driver.quit();
 	}
 
 }
